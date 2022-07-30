c56fbb4e444a4d8816635714e30557547c64a9b5
“ABearDD” <abear@drunkendragon.games>
2022-07-30 14:48:55 -0500

    pad added

--- console.py
+++ console.py
@@ -1,13 +1,31 @@
 import curses
 from curses import wrapper
+from random import randint
+import time
+
+MAX_COLS = 500
+MAX_ROWS = 500
 
 def main(stdscrn):
-    stdscrn.clear()
+    # stdscrn.clear()
+    # stdscrn.refresh()
+    # stdscrn.addstr(90, 90, "hello world")
+    # time.sleep(100000)
     rows, cols = stdscrn.getmaxyx()
-    stdscrn.addstr(rows,cols,f"{rows} {cols}")
+    pad = curses.newpad(MAX_COLS, MAX_ROWS)
     stdscrn.refresh()
+    stdscrn.addstr(10, 10, f"{rows},{cols}")
+
+    for i in range(MAX_COLS - 1):
+        for j in range(MAX_ROWS - 1):
+            char = chr(randint(65, 65+25))
+            pad.addstr(char)
+
+    for i in range(50):
+        pad.refresh(0, 0, i, 0, rows-1, cols-1)
+        time.sleep(0.2)
+        stdscrn.clear()
+        stdscrn.refresh()
     stdscrn.getch()
 
-window = curses.initscr()
-window.resize(45, 200)
 wrapper(main)
