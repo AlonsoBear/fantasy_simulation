cb2cc07973cc1bf26705ed1573ac395185f33904
AlonsoBear <alonsobearb@gmail.com>
2022-07-30 22:51:32 -0500

    movment improved

--- src/npcs/animal.py
+++ src/npcs/animal.py
@@ -1,6 +1,7 @@
 from settings import MAX_COLS, MAX_ROWS, FISH, MANZANAS, ARBOL_DE_MANZANA, GRASS
 from random import randint, uniform
-import curses
+import curses,logging
+
 def charToColor(prev):
     if prev == 'Y':
          return 1
@@ -17,10 +18,10 @@ def charToColor(prev):
     return 1
 class Animal():
     def __init__(self, pad):
-        # self.x = randint(0,MAX_COLS - 1)
-        # self.y = randint(0,MAX_ROWS - 1)
-        self.x = 30
-        self.y = 25
+        self.x = randint(0,MAX_COLS - 1)
+        self.y = randint(0,MAX_ROWS - 1)
+        # self.x = 30
+        # self.y = 25
         attrs = pad.inch(self.x, self.y)
         self.previous = chr(attrs & 0xFF)
         self.hp = 200
@@ -45,25 +46,15 @@ class Animal():
     def move_to(self):
         new_x = self.x + randint(-1, 1)
         new_y = self.y + randint(-1, 1)
-        if(new_x > 499 or new_x < 1 or new_y > 499 or new_x < 1):
+        if(new_x > 499 or new_x < 1 or new_y > 499 or new_y < 1):
             return self.x, self.y
         return new_x, new_y
     
     def cycle(self):
         if(self.is_dead()):
             return
-        new_x = self.x + randint(-1, 1)
-        new_y = self.y + randint(-1, 1)
+        new_x, new_y = self.move_to()
 
-        if new_x > 499:
-            new_x = 499
-        if new_y > 499:
-            new_y = 499
-        if new_x < 1:
-            new_x = 1
-        if new_y < 1:
-            new_y = 1
-        
         if(new_x != self.x or new_y != self.y): self.move(new_x,new_y)
 
     def is_dead(self):
